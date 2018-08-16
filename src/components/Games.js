49bb61931445ceaeb2d42db71386c582beeb86ac
nerijus-ubuntu <nerijusanz@gmail.com>
2018-08-16 14:26:25 +0300

    added package prop-types

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
+import GamesList from './GamesList';
 
 class Games extends Component {
   render() {
     return (
-      <h1>Games</h1>
+      <div>
+        <h1>Games</h1>
+        <GamesList games={this.props.games}/>
+      </div>
+
     );    
   }
 }
 
-export default Games;
+
+Games.propTypes = {
+  games: PropTypes.array.isRequired
+}
+
+//get reducers state
+function mapStateToProps(state){
+  return{
+    games: state.games
+  }
+}
+
+export default connect(mapStateToProps)(Games);
